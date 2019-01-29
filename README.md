f58cceabc74020238232b150897bc274fcd796f1
37712 <cgr37712@gmail.com>
2019-01-29 00:40:51 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+CS310 Spring 2018
+Data Structures
+# Generic_ArrayPriorityQueue
+SDSU CS310 Spring 2018 Programming Assignment #2
+
+For this assignment, you will write two more implementations of a Priority Queue. Using the same interface as program #1, you will write two linked listed implementations.
+Your implementations will be:	
+    1. Ordered Singly Linked List	
+    2. Unordered Singly Linked List		
+Both implementations must have identical behavior, and must implement the PriorityQueue interface used in program #1. The implementations must have a single no-argument contructor. As linked lists are never full, there is no default size or maximum size.
+
+Thus, your project will consist of the following files. You must use exactly these filenames.	
+    • PriorityQueue.java The ADT interface (provided in prog1)	
+    • OrderedLinkedListPriorityQueue.java The ordered list implementation.	
+    • UnorderedLinkedListPriorityQueue.java The unordered list implementation.	
+Additional Details:	
+    • Each method must be as efficient as possible. That is, a O(n) is unacceptable if the method could be written with O(log n) complexity.	
+    • You may not make any modifications to the PriorityQueue interface provided. I will grade your project with my copy of this file. This interface is UNCHANGED from project #1	
+    • All relevant requirements from the first assignment apply here. Of course, arrays will have a limited size but linked lists do not. Thus, all references to maximum size are not relevant for this assignment.	
+    • The isFull() method should be hardcoded to return false.	
+    • You should ignore (but do not remove) the DEFAULT_MAX_CAPACITY variable in the interface.	
---
+++ data_structures/UnorderedLinkedListPriorityQueue.java
@@ -0,0 +1,157 @@
+
+/*  CS310 Spring 2018
+    Programming Assignment #2
+    Carlos A. Gamino Reyes
+*/
+
+/*  The PriorityQueue ADT may store objects in any order.  However,
+    removal of objects from the PQ must follow specific criteria.
+    The object of highest priority that has been in the PQ longest
+    must be the object returned by the remove() method.  FIFO return
+    order must be preserved for objects of identical priority.
+   
+    Ranking of objects by priority is determined by the Comparable<E>
+    interface.  All objects inserted into the PQ must implement this
+    interface.
+*/
+
+package data_structures;
+
+import java.util.Iterator;
+
+public class UnorderedLinkedListPriorityQueue
+        <E extends Comparable <E>> implements PriorityQueue <E>{
+    
+    private Node<E> head;
+    private int size;
+    
+    public UnorderedLinkedListPriorityQueue(){
+        head = null;
+        size = 0;
+    }
+    
+    private class Node<E> {
+        private E data;
+        private Node next;
+        
+        public Node(E obj) {
+            data = obj;
+            next = null;
+        }
+    }
+    
+    //  Inserts a new object into the priority queue.  Returns true if
+    //  the insertion is successful.  If the PQ is full, the insertion
+    //  is aborted, and the method returns false.
+    public boolean insert(E obj){
+        Node<E> tmp = new Node<E>(obj);
+        tmp.next= head;
+        head = tmp;
+        size++;
+        return true;
+    }   
+   
+    //  Removes the object of highest priority that has been in the
+    //  PQ the longest, and returns it.  Returns null if the PQ is empty.
+    public E remove(){
+        if(isEmpty()) return null;
+        Node<E> minPrev = null, minPtr = null, prev = null, ptr = head;
+        E min = head.data;
+        while(ptr != null){
+            if(ptr.data.compareTo(min) <= 0){
+                min = ptr.data;
+                minPrev = prev;
+                minPtr = ptr;
+            }
+            prev = ptr;
+            ptr = ptr.next;
+        }
+        if(minPrev == null) head = head.next;
+        else{
+            minPtr = minPtr.next;
+            minPrev.next = minPtr;
+        }
+        size--;
+        return min;
+    }
+    
+    //  Deletes all instances of the parameter obj from the PQ if found, and
+    //  returns true.  Returns false if no match to the parameter obj is found.
+    public boolean delete(E obj){
+        Node<E> prev = null, ptr = head;
+        boolean flag = false;
+        while(ptr != null){
+            if(obj.compareTo(ptr.data) == 0){//found??
+                if(ptr == head)//ptr is at the head    
+                        head = head.next;
+                else{//ptr is not at the head
+                    prev.next = ptr.next;
+                    ptr = prev;
+                }
+                size--;
+                flag = true;
+            }
+            prev = ptr;
+            ptr = ptr.next;
+        }
+        return flag;
+    }
+    
+    //  Returns the object of highest priority that has been in the
+    //  PQ the longest, but does NOT remove it. 
+    //  Returns null if the PQ is empty.
+    public E peek(){
+        if(isEmpty()) return null;
+        Node<E> ptr = head.next;
+        E min = head.data;
+        while(ptr != null){
+            if(ptr.data.compareTo(min) <= 0) min = ptr.data;
+            ptr = ptr.next;
+        }
+        return min;
+    }   
+    
+    //  Returns true if the priority queue contains the specified element
+    //  false otherwise.
+    public boolean contains(E obj){
+        Node<E> ptr = head;
+        while(ptr != null){
+            if(obj.compareTo(ptr.data) == 0)return true;
+            ptr = ptr.next;
+        }
+        return false;
+    } 
+   
+    //  Returns the number of objects currently in the PQ.
+    public int size(){ return size; }
+      
+    //  Returns the PQ to an empty state.
+    public void clear(){ head = null; size = 0; }
+   
+    //  Returns true if the PQ is empty, otherwise false
+    public boolean isEmpty(){ return size == 0; }
+   
+    //  Returns true if the PQ is full, otherwise false.  List based
+    //  implementations should always return false.
+    public boolean isFull(){ return false; }  
+    
+    //  Returns an iterator of the objects in the PQ, in no particular
+    //  order.  
+    public Iterator<E> iterator(){ return new IteratorHelper(); }
+    
+    private class IteratorHelper implements Iterator<E>{
+        
+        private Node<E> ptr = head;
+        
+        public E next(){
+            E tmp = ptr.data;
+            ptr = ptr.next;
+            return tmp;
+        }
+        
+        public boolean hasNext(){ return ptr != null; }
+        
+        public void remove(){ throw new UnsupportedOperationException(); }
+        
+    }
+}
